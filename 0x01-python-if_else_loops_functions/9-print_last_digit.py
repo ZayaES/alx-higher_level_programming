64139ff833b533ce9f72c14740b28877a7c29b44
ZayaES <heavenphrince@gmail.com>
2023-01-04 05:11:48 -0800

    2nd test of task9

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -4,7 +4,6 @@ def print_last_digit(number):
         last_dig = number % 10
     else:
         last_dig = 10 - (number % 10)
-
     print("{}".format(last_dig), end="")
     return last_dig
         
