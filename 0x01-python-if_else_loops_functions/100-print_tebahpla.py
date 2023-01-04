a270c42b5eb19072f9125d104621eeb86656f48d
ZayaES <heavenphrince@gmail.com>
2023-01-04 06:06:35 -0800

    1st test of task100

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 i = 0
 for alp in reversed(range(97, 123)):
-        if i % 2 == 0:
-            print("{}".format(chr(alp)), end="")
-        else:
-            print("{}".format(chr(alp - 32)), end="")
-        i += 1
+    if i % 2 == 0:
+        print("{}".format(chr(alp)), end="")
+    else:
+        print("{}".format(chr(alp - 32)), end="")
+    i += 1
