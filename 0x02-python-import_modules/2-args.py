679d06cb07c5301e9167eb5ebcabe279460b7557
ZayaES <heavenphrince@gmail.com>
2023-01-05 17:24:21 -0800

    3rd teat of task2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,10 +2,12 @@
 if __name__ == "__main__":
     from sys import argv
 x = len(argv) - 1
-if x != 1:
+if x == 0:
     print("{} arguments.".format(x))
 elif x == 1:
     print("{} argument:".format(x))
+else:
+    print("{} arguments:".format(x))
 y = 1
 for i in argv[1:]:
     print("{}: {}".format(y, i))
