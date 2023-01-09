c9c03ca00a4a99ae99fe068087e81c95b4ab5f1e
ZayaES <heavenphrince@gmail.com>
2023-01-09 07:19:44 -0800

    3rd test of task9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-def max_integer(my_list=[]):
+def max_integer(my_list=[]):
     """Returns largest int of a list"""
-    if my_list:
+    if my_list:
         my_list.sort()
-        return my_list[-1]
+        return my_list[-1]
     else:
-        return None
+        return None
