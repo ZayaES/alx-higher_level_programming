eacb566f7b973c4692535d7628e04653c24ffbcb
ZayaES <heavenphrince@gmail.com>
2023-01-10 09:45:39 -0800

    new test of task1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-def element_at(my_list, idx):
-    return None if idx < 0 or idx >= len(my_list) else my_list[idx]
+def element_at(my_list, idx):
+    if idx < 0 or idx >= len(my_list):
+        return None
+    else my_list[idx]
