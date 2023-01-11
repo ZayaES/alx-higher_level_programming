06bf6043caa4ef7e4b860606a0f9778ded05d0a4
ZayaES <heavenphrince@gmail.com>
2023-01-11 09:11:32 -0800

    2nd test of task1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     """ changes entries of a list"""
-    f = lambda x: replace if (x == search) else x
-    return list(map(f, my_list))
+    return list(map(lambda x: replace if (x == search) else x, my_list))
