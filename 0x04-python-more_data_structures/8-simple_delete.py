ec1b300d8cc5a326a1db332671d62f223f90f451
ZayaES <heavenphrince@gmail.com>
2023-01-11 10:17:24 -0800

    1st test of task8

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    if a_dictionary[key] is None:
+    if a_dictionary.get(key) is None:
         return a_dictionary
     del a_dictionary[key]
     return a_dictionary
