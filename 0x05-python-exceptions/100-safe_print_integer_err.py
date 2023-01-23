9fb0fcec43a26d05456d73acb7e65bedb175ea76
ZayaES <heavenphrince@gmail.com>
2023-01-23 11:57:06 -0800

    2nd test of task7

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
+import sys
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
     except Exception as esp_mes:
-        print("Exception:",esp_mes)
+        print("Exception: {}".format(esp_mes), file=sys.stderr);
         return False
