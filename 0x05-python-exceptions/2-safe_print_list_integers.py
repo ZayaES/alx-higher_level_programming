07b982475735b0aea76c414ad8f271205767db2b
ZayaES <heavenphrince@gmail.com>
2023-01-23 08:09:24 -0800

    1st test of task2

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    i = 0
+    j = 0
+    while (i < x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+        except (ValueError, TypeError):
+            j += 1
+            pass
+        i += 1
+    print()
+    return i - j
