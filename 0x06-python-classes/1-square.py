a6a3138963cc9ee7786b60734ff807f89db0f905
ZayaES <heavenphrince@gmail.com>
2023-01-24 12:46:48 -0800

    1st test of task1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""This is a module that creates a class 
+based on 0-square.py
+
+"""
+
+class Square(object):
+    """A class that takes an input
+
+    """
+    def __init__(self, size):
+        """the init module which initializes
+        the instance of the class
+
+        """
+        self.__size = size
