42db37467a142055a48ec0ba4ced9de0456d5a92
ZayaES <heavenphrince@gmail.com>
2023-01-24 16:43:12 -0800

    1st commit for task5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+"""This is a module that creates a class
+based on 3-square.py
+
+"""
+
+
+class Square(object):
+    """A class that takes an input
+
+    """
+    def __init__(self, size=0):
+        """the init module which initializes
+        the instance of the class
+
+        """
+        self.__size = size
+        try:
+            range(0, self.__size)
+        except Exception:
+            raise TypeError("size must be an integer")
+        else:
+            if int(self.__size < 0):
+                raise ValueError("size must be >= 0")
+
+    def area(self):
+        """Returns the area of the square given the size
+
+        """
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """a method which retrieves the instance private
+        instance attribute size
+
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """sets the value of the instance private
+        attribute size
+
+        """
+        try:
+            range(0, value)
+        except Exception:
+            raise TypeError("size must be an integer")
+        else:
+            if int(value < 0):
+                raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def my_print(self):
+        """Prints the square using the # character
+
+        """
+        if self.__size == 0:
+            print()
+        for i in range(0, self.__size):
+            print("#" * self.__size)
