19a7684ca8cb8042b6cadc87ce263fad0b60f48d
ZayaES <heavenphrince@gmail.com>
2023-01-24 17:19:29 -0800

    2nd commit for task6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -76,11 +76,8 @@ class Square(object):
         """
         if self.__size == 0:
             print()
-        for i in range(0, self.__size):
-            print("#" * self.__size)
-
         [print("") for i in range(0, self.__position[1])]
         for i in range(0, self.__size):
-            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("_", end="") for j in range(0, self.__position[0])]
             [print("#", end="") for k in range(0, self.__size)]
             print("")
