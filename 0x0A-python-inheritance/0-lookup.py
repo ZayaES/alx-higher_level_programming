c8f26824c913e5f3b774051909d76726095fb006
ZayaES <heavenphrince@gmail.com>
2023-02-07 01:21:39 -0800

    1st test of task1

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+#Author: Zaya Bell
+"""Defines a lookup funtion for objects."""
+
+
+def lookup(obj):
+    """Returns a list of attribute of class object"""
+    return(dir(obj))
---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+#Author: Zaya Bell
+"""Defines a class MyList which inherits from class List"""
+
+
+class MyList(List):
+    """Defines MyList as a List"""
+
+    def print_sorted(self):
+        """sorts out list in ascending order"""
+
+        print(sorted(self))
+        
