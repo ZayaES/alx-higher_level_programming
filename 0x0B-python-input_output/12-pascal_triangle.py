36ad756bfdabbdbaf51a2a3279ab55c2c1427219
ZayaES <heavenphrince@gmail.com>
2023-02-09 16:07:12 -0800

    Pascal triangle

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,16 @@
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+    if n == 1:
+        return [1]
+    if n == 2:
+        return [[1], [1, 1]]
+    
+    i = 0
+    triangle = [[1], [1, 1]]
+    for j in range(3, n + 1):
+        row_n = [1]
+        row_n.extend(triangle[-1][i] + triangle[-1][i+1] for i in range(j - 2))
+        row_n.append(1)
+        triangle.append(row_n)
+    return triangle
