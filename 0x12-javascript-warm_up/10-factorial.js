1d37420f6143244ab4addebea2acc52e93a23a0e
ZayaES <heavenphrince@gmail.com>
2023-04-12 16:32:26 -0700

    task10 semistandard complaiance

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -4,11 +4,9 @@ function factorial (a) {
   if (Number.isNaN(a) || (a === 0) || (a === 1)) {
     return 1;
   } else {
-    return (a * factorial (a - 1));
+    return (a * factorial(a - 1));
   }
 }
 
 const ans = factorial(parseInt(process.argv[2]));
-console.log(ans)
-
-
+console.log(ans);
