48595a64d64fac686448d2f5dd19a102d3a9e384
ZayaES <heavenphrince@gmail.com>
2023-04-12 15:10:44 -0700

    task5 not a number fix

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,7 +2,7 @@
 
 const num = parseInt(process.argv[2]);
 if (Number.isNaN(num)) {
-  console.log('My number: Not a number');
+  console.log('Not a number');
 } else {
   console.log(`My number: ${num}`);
 }
