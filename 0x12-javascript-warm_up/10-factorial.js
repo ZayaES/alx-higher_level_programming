84ac55337f656165a969df5810fca3e3a8096867
ZayaES <heavenphrince@gmail.com>
2023-04-12 16:29:10 -0700

    task9-10 test0

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+function factorial (a) {
+  if (Number.isNaN(a) || (a === 0) || (a === 1)) {
+    return 1;
+  } else {
+    return (a * factorial (a - 1));
+  }
+}
+
+const ans = factorial(parseInt(process.argv[2]));
+console.log(ans)
+
+
