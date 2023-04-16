b8c6ffbaa58bee3eb288da01a5bc4e5dee1a381f
ZayaES <heavenphrince@gmail.com>
2023-04-15 23:13:18 -0700

    task10 without declaring new variavle

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -5,17 +5,7 @@ exports.converter = function (base) {
     if (base === 10) {
       return number;
     } else {
-      let result = '';
-      const letDigit = ['a', 'b', 'c', 'd', 'e', 'f'];
-      while (number !== 0) {
-        let rem = number % base;
-        if (rem >= 10) {
-          rem = letDigit[rem % 10];
-        }
-        result += rem;
-        number = Math.floor(number / base);
+      return (number.toString(base));
       }
-      return [...result].reverse().join('');
     }
-  };
-};
+  }
