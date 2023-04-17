bc45d9bcae3ad811b960deec9798880159f8786e
ZayaES <heavenphrince@gmail.com>
2023-04-17 09:27:01 -0700

    task3 (no to sql injection) test0

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""List all states from the database hbtn_0e_0_usa"""
+
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3])
+
+    i = 0
+    cur = db.cursor()
+    state_name = sys.argv[4]
+    print(sys.argv[4][1])
+    while(sys.argv[4][i] != ';' and sys.argv[4][i] != ' '):
+        if (i == len(sys.argv[4]) - 1):
+            break
+        i += 1
+    """if (sys.argv[4][1] != None):
+        exit()"""
+    query_temp = "SELECT * FROM states WHERE name= '{}' ORDER BY id ASC"
+    query = query_temp.format(state_name)
+    cur.execute(query)
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
