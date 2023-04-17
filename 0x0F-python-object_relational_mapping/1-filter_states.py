727272f6b1dd0d5fca0e7298e4acfb8ff5efce6c
ZayaES <heavenphrince@gmail.com>
2023-04-17 06:52:50 -0700

    cannot run when imported

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,25 +1,26 @@
 #!/usr/bin/python3
 """List all states from the database hbtn_0e_0_usa"""
 
-import MySQLdb
-import sys
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
 
-db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3])
+    db = MySQdb.connect(
+            host="localhost",
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3])
 
-cur = db.cursor()
-starting_letter = 'N'
-cur.execute(
-    "SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC",
-    (starting_letter + '%',))
-rows = cur.fetchall()
+    cur = db.cursor()
+    starting_letter = 'N'
+    cur.execute(
+        "SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC",
+        (starting_letter + '%',))
+    rows = cur.fetchall()
 
-for row in rows:
-    print(row)
+    for row in rows:
+        print(row)
 
-cur.close()
-db.close()
+    cur.close()
+    db.close()
