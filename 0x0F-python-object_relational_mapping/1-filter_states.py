6c426c371d818f1b72c4846176c46eace2182cba
ZayaES <heavenphrince@gmail.com>
2023-04-17 05:04:25 -0700

    task0-1 test0

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""List all states from the database hbtn_0e_0_usa"""
+
+import MySQLdb
+import sys
+
+db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3])
+
+cur = db.cursor()
+starting_letter = 'N'
+cur.execute(
+    "SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC",
+    (starting_letter + '%',))
+rows = cur.fetchall()
+
+for row in rows:
+    print(row)
+
+cur.close()
+db.close()
---
+++ 0x0F-python-object_relational_mapping/README.md
@@ -0,0 +1,2 @@
+Alx project on MySQLdb and SQLAlchemy  
+This would be a tough one
