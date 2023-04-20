d8450be228f013a99181fdb2724e2d9e44e9125e
ZayaES <heavenphrince@gmail.com>
2023-04-20 14:05:09 -0700

    task6 pycodestyle compliance

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -7,8 +7,8 @@ from sqlalchemy import Integer, Column, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-
 Base = declarative_base()
+
 
 class State(Base):
     """ State class which creates a table state in MySQL database"""
