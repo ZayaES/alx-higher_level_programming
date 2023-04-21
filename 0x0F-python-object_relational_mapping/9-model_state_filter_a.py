3f75f6aa5dbd1ab39b671d519516728c00417c9c
ZayaES <heavenphrince@gmail.com>
2023-04-21 08:20:16 -0700

    task9 test0

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Connects to a dtabase and prints thr table states"""
+
+if __name__ == "__main__":
+    from sqlalchemy import Integer, String, MetaData, create_engine
+    from sqlalchemy.orm import Session, sessionmaker
+    import sys
+    from model_state import Base, State
+
+    user = sys.argv[1]
+    passwd = sys.argv[2]
+    dbase = sys.argv[3]
+    engine = create_engine(
+            "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+                user, passwd, dbase))
+
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    a_states = session.query(State).filter(State.name.like("%a%"))
+    for state in a_states:
+        print("{}: {}".format(state.id, state.name))
