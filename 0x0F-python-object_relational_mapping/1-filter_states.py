b7a761bcd40793391f47feebff25b8156b990e30
Zaya Bell <heavenphrince@gmail.com>
2023-06-19 14:31:13 -0700

    task2,1 cor

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    query = ("SELECT * FROM `states` WHERE `name` LIKE %s")
+    query = ("SELECT * FROM `states` WHERE `name` LIKE %s ORDER BY id")
     parameter = ("N%", )
     try:
         cur.execute(query, parameter)
--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -10,12 +10,12 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    query = ("SELECT * FROM states
-             WHERE name LIKE {}
-             ORDER BY id".format(parameter))
-    parameter = (sys.argv[4],)
+    parameter = (sys.argv[4])
+    #query = ()
     try:
-        cur.execute(query, parameter)
+        cur.execute("SELECT * FROM states \
+                    WHERE BINARY name = '{}' \
+                    ORDER BY id".format(parameter))
         rows = cur.fetchall()
     except MySQLdb.Error as e:
         print(str(e))
