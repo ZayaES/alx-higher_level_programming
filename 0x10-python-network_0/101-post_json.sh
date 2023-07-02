25e6c10d0796d5a3852784b0c71a0df8a003ad43
Zaya Bell <heavenphrince@gmail.com>
2023-07-02 19:55:03 +0000

    task8 test2

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends json post request and display body
-curl -s -X POST -H "Content-Type: application/json" -F "file=$2" "$1"
+curl -s -X POST -H "Content-Type: application/json" -d "$(cat "$2")" "$1"
