511e43167c4710c27028ebc96e551cb349056252
Zaya Bell <heavenphrince@gmail.com>
2023-08-04 21:12:32 +0000

    task6 test1

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -2,24 +2,22 @@
 
 const request = require('request');
 const url = process.argv[2];
-var c_tasks = {};
-var count = 0;
+const cTasks = {};
 
 request(url, function (err, response, body) {
   if (err) {
-  console.error(err)
+    console.error(err);
   }
   body = JSON.parse(body);
-  len = body.length;
 
-  for (var task of body) {
+  for (const task of body) {
     if (task.completed === true) {
-      if (c_tasks[task.userId]) {
-        c_tasks[task.userId] += 1;
+      if (cTasks[task.userId]) {
+        cTasks[task.userId] += 1;
       } else {
-        c_tasks[task.userId] = 1;
+        cTasks[task.userId] = 1;
       }
     }
   }
-  console.log(c_tasks);
+  console.log(cTasks);
 });
