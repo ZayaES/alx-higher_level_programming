379cd65be463397cc2edf35467f6b57d105949cb
Zaya Bell <heavenphrince@gmail.com>
2023-08-04 20:25:43 +0000

    task5 test1

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const request = require('request');
+const fs = require('fs');
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+function readWrite(url, filePath) {
+  request(url).pipe(fs.createWriteStream(filePath))
+};
+
+readWrite(url, filePath);
